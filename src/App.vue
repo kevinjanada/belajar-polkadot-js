bda783eb004a2d378adfbd8383429dec45c366a6
Kevin <kevin.janada@gmail.com>
2021-04-04 20:05:25 +0700

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,62 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Balance: {{ balanceFree }}
+    nonce: {{ nonce }}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { mnemonicGenerate } from '@polkadot/util-crypto'
+import { Keyring } from '@polkadot/keyring';
+import { ApiPromise, WsProvider } from '@polkadot/api';
+import types from './types.json'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data: () => ({
+    mnemonic: '',
+    pair: null,
+    api: null,
+    balanceFree: null,
+    nonce: 0,
+  }),
+  async mounted() {
+    this.generateAccount()
+    await this.connectToBlockchain()
+    await this.getBalance(
+      '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY'
+    )
+  },
+  methods: {
+    generateAccount() {
+      const keyring = new Keyring({ type: 'ed25519', ss58Format: 42 })
+
+      const mnemonic = mnemonicGenerate()
+      this.mnemonic = mnemonic
+
+      const pair = keyring.addFromUri(mnemonic, { name: 'first pair' }, 'ed25519');
+      this.pair = pair
+
+      console.log(this.pair)
+    },
+    async connectToBlockchain() {
+      const wsProvider = new WsProvider('wss://debio.theapps.dev/node');
+      const api = await ApiPromise.create({
+        provider: wsProvider,
+        types: types
+      });
+      this.api = api
+    },
+    async getBalance(addr) {
+      const { nonce, data: balance } = await this.api.query.system.account(addr);
+      console.log(nonce)
+      console.log(balance)
+      this.nonce = nonce
+      this.balanceFree = balance.free
+      console.log(`balance of ${balance.free} and a nonce of ${nonce}`);
+    }
   }
 }
 </script>
